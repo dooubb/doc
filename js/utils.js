c25260e742d19e973f44238039959082eda011d6
fanqiang <oge@live.com>
2024-07-08 21:51:44 +0800

    Site updated: 2024-07-08 21:51:34

--- js/utils.js
+++ js/utils.js
@@ -24,7 +24,8 @@ NexT.utils = {
         $imageWrapLink.attr('data-fancybox', 'default').attr('rel', 'default');
       }
 
-      var imageTitle = $image.attr('title') || $image.attr('alt');
+	  //var imageTitle = $image.attr('title') || $image.attr('alt'); 
+      var imageTitle =  $image.attr('alt');
       if (imageTitle) {
         $imageWrapLink.append(`<p class="image-caption">${imageTitle}</p>`);
         // Make sure img title tag will show correctly in fancybox
